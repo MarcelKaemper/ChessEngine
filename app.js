afe365b2a2ba91cda0c2b5a30c918ade9f581417
Marcel <marcelkaemper862@gmail.com>
2019-06-21 00:56:40 +0200

    Improved pawn movement

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ let knight;
 
 preload = () => {
 	rook = loadImage(JSON.parse(pieces).R);
-	king = loadImage(JSON.parse(pieces).E);
+	king = loadImage(JSON.parse(pieces).K);
 	queen = loadImage(JSON.parse(pieces).Q);
 	pawn = loadImage(JSON.parse(pieces).P);
 	bishop = loadImage(JSON.parse(pieces).B);
-	knight = loadImage(JSON.parse(pieces).K);
+	knight = loadImage(JSON.parse(pieces).N);
 }
 
 
@@ -69,14 +69,24 @@ mouseReleased = () => {
 	let moveDistance = [moveT[0]-moveD[0],moveT[1]-moveD[1]];
 
 	if(toMove.p != null){
-		if(moveTo.p == null){
-			if(toMove.p == "P"){
+		if(toMove.p == "P"){
+			if(moveTo.p == null){
 				console.log(moveDistance);
-				if((toMove.color == 2 && moveDistance[0] == -2 || moveDistance[0] == -1)
-				|| (toMove.color == 1 && moveDistance[0] == 2|| moveDistance[0] == 1)) {
+				console.log(toMove);
+				if((moveDistance[0] == 1 && toMove.color == 1 && moveDistance[1] == 0)
+					|| moveDistance[0] == -1 && toMove.color == 2 && moveDistance[1] == 0){
 					cells[moveD[0]][moveD[1]].p = toMove.p;
 					cells[moveT[0]][moveT[1]].p = null;
+
+					cells[moveD[0]][moveD[1]].color = toMove.color;
+					cells[moveT[0]][moveT[0]].color = 0;
 				}
+			// }else{
+			// 	console.log("else");
+			// 	console.log(moveDistance);
+			// 	if(moveDistance[1] == -1 || moveDistance[1] == 1){
+			// 		console.log("Yeet");
+			// 	}
 			}
 		}
 	}
