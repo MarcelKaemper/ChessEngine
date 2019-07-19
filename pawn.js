b4dc2bd08416def2de28530d8bc1f8f6e54c31f8
MarcelKaemper <marcelkaemper862@gmail.com>
2019-07-19 21:23:40 +0200

    Added pawn movement

--- pawn.js
+++ pawn.js
@@ -1,8 +1,31 @@
 class Pawn extends Piece{
 	constructor(x,y){
-		super(x,y,loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/1024px-Chess_pdt45.svg.png",() => {
+		super(x,y,loadImage(pieces.P,() => {
 			this.show();
 			console.log(this.x+" | "+this.y);	
 		}));
+	}
+
+	move = (toX,toY) => {
+		let diffY = this.y - toY;
+		let diffX = this.x - toX;
+		console.log(diffY,diffX);
+
+		if(cells[toX][toY].p == null && abs(diffX) == 0){
+			if(this.color == 1 && diffY<=0 || this.color == 0 && diffY>=0){
+				if(abs(diffY) <= 1 || abs(diffY) == 2 && this.firstMove == 1){
+					this.firstMove = 0;
+					this.confMove(toX,toY);
+				}
+			}
+		}else if(cells[toX][toY].p != null && abs(diffX)<=1){
+			if(cells[toX][toY].p.color != this.color){
+				if(this.color == 1 && diffY<=0 || this.color == 0 && diffY>=0){
+					this.firstMove = 0;
+					// destroy piece
+					this.confMove(toX, toY);
+				}
+			}
+		}
 	}
 }
--- piece.js
+++ piece.js
@@ -3,14 +3,16 @@ class Piece{
 		this.x = x;
 		this.y = y;
 		this.image = image;
+		this.color = 1;
+		this.firstMove = 1;
 		cells[x][y].p = this;
 	}
 
 	show = () => {
-		image(this.image, this.x*cellSize,this.y*cellSize, cellSize, cellSize)
+		image(this.image, this.x*cellSize+32,this.y*cellSize, cellSize, cellSize)
 	}
 
-	move = (targetX, targetY) => {
+	confMove = (targetX, targetY) => {
 		cells[this.x][this.y].p = null;
 		this.x = targetX;
 		this.y = targetY;
